07fc3bb50cf856ed99b24e94e46d98310504ff2c
EleganceLESS <eleganceless@gmail.com>
2015-12-23 03:07:18 +0900

    First commit

---
+++ StarbuzzCoffee.java
@@ -0,0 +1,6 @@
+public class StarbuzzCoffee{
+    public static void main(String args[]){
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+    }
+}
